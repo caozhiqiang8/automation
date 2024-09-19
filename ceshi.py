fe4c32effeb172b2e098c5fda2b5a36c1f74ce5d
czq411127520 <411127520@qq.com>
2024-09-20 00:35:00 +0800

    'ai'

--- ceshi.py
+++ ceshi.py
@@ -1,30 +1,37 @@
 import sqlite3
 import os
+from DrissionPage import ChromiumPage
+from DrissionPage.common import Actions,Keys
+import re
 
-#
-# file_path = os.getcwd()
-# hostname = os.popen('hostname').read().strip()
-#
-# conn = sqlite3.connect('automation.db')
-# cursor = conn.cursor()
-# sql = '''
-# select * from host_key where  host='{}' and key != ''
-# '''.format(hostname)
-#
-# result = cursor.execute(sql).fetchall()
-#
-# cursor.close()
-# conn.close()
-#
-# print(result)
-# if  result  :
-#     print('有数据')
-# else:
-#     print("不存在")
-#
-with open(r'文章链接.txt', 'r', encoding='utf-8') as file:
-    cdk = file.read()
+# page = ChromiumPage()
+# url = 'https://a.mp.uc.cn/article.html?uc_param_str=frdnsnpfvecpntnwprdssskt&wm_aid=630a5e57226e49b0b5c60ec8fdd09517'
+# page.get(url)
+# connect = page.ele('xpath://*[@id="react_app_content_id"]/div[3]/div[2]').text
+# title = page.ele('xpath://*[@id="react_app_content_id"]/div[3]/h1').text
+# img = page.ele('xpath://*[@id="react_app_content_id"]/div[3]/div[2]').eles('tag:img')
+# imgList = img.get.links()
+# print(title)
+# print(connect)
+# print(imgList)
 
-print(cdk)
-urlList = cdk.splitlines()
-print(urlList)
+# page.get(url)
+# connect = page.ele('xpath://*[@id="react_app_content_id"]/div/div[4]/div[2]/p[1]').text
+# title = connect[:30]
+# imgFather = page.ele('xpath://*[@id="gridContainer"]').children()
+# imgList = []
+# for i in imgFather:
+#     img = i.child().child().attr('style')
+#     print(img)
+#     zhengze = r'"([^"]*)"|'r'\'([^\']*)\''
+#     imgZhengZe = [m[0] for m in (re.findall(zhengze, img)) if m[0]]
+#     imgList.append(imgZhengZe[0])
+
+
+result = '(相似度:23.4%)'
+result = float(result[5:-2])
+print(result)
+if result < 25.00:
+    print('通过')
+else:
+    print('不通过')
--- toutiao.py
+++ toutiao.py
@@ -34,7 +34,6 @@ def secretKey():
     cursor.close()
     conn.close()
 
-
 def getArticleUrl(navItem,scrollNum = 0):
     page.get('https://www.toutiao.com/')
     page.wait(1)
@@ -63,124 +62,181 @@ def getArticleUrl(navItem,scrollNum = 0):
 
 def getArticle(url):
     print('[{}] 开始获取文章内容......'.format(nowTime()))
-    if 'https://www.toutiao.com' in url :
-        page.get(url)
-        page.wait(1)
-        connect = page.ele('tag:article').text
-        page.wait(1)
-        title = page.ele('@class=article-content').ele('tag:h1').text
-        page.wait(1)
-        img = page.ele('tag:article').eles('tag:img')
-        imgList = img.get.links()
-        page.wait(1)
+    try:
+        if 'https://www.toutiao.com' in url :
+            page.get(url)
+            connect = page.ele('xpath://*[@id="root"]/div[2]/div[2]/div[1]/div/div/div/div/article').text
+            title = page.ele('@class=article-content').ele('tag:h1').text
+            img = page.ele('xpath://*[@id="root"]/div[2]/div[2]/div[1]/div/div/div/div/article').eles('tag:img')
+            imgList = img.get.links()
+        elif 'https://www.163.com/' in url:
+            page.get(url)
+            connect = page.ele('xpath://*[@id="content"]/div[2]').text
+            title = page.ele('@class=post_title').text
+            img = page.ele('@class:post_body').eles('tag:img')
+            imgList = img.get.links()
+        elif 'https://www.sohu.com/' in url :
+            page.get(url)
+            connect = page.ele('@id=mp-editor').text
+            title = page.ele('@class=text-title').text
+            img = page.ele('@id=mp-editor').eles('tag:img')
+            imgList = []
+            for i in img:
+                imgurl = i.attr('src')
+                if imgurl == None:
+                    imgurl = r'https:' + i.attr('data-src')
+                imgList.append(imgurl)
+        elif 'https://new.qq.com/' in url:
+            page.get(url)
+            connect = page.ele('xpath://*[@id="ArticleContent"]/div[2]/div').text
+            title = page.ele('xpath://*[@id="dc-normal-body"]/div[3]/div[1]/div[1]/div[2]/h1').text
+            img = page.ele('xpath://*[@id="ArticleContent"]/div[2]/div').eles('tag:img')
+            imgList = img.get.links()
+        elif 'https://a.mp.uc.cn/' in url :
+            page.get(url)
+            connect = page.ele('xpath://*[@id="react_app_content_id"]/div[3]/div[2]').text
+            title = page.ele('xpath://*[@id="react_app_content_id"]/div[3]/h1').text
+            img = page.ele('xpath://*[@id="react_app_content_id"]/div[3]/div[2]').eles('tag:img')
+            imgList = img.get.links()
+        elif 'https://www.360kuai.com/'in url :
+            page.get(url)
+            connect = page.ele('@id=article__content').text
+            title = page.ele('@class=article__title').text
+            img = page.ele('@id=article__content').eles('tag:img')
+            imgList = img.get.links()
+        else:
+            return 5,5,5
         print('[{}] 文章标题、内容、图片获取成功'.format(nowTime()))
         return connect, title, imgList[:8]
-    elif 'https://www.163.com/' in url:
-        page.get(url)
-        page.wait(1)
-        connect = page.ele('xpath://*[@id="content"]/div[2]').text
-        page.wait(1)
-        title = page.ele('@class=post_title').text
-        page.wait(1)
-        img = page.ele('@class:post_body').eles('tag:img')
-        imgList = img.get.links()
-        page.wait(1)
-        print('[{}] 文章标题、内容、图片获取成功'.format(nowTime()))
-        return connect, title, imgList[:8]
-    elif 'https://www.sohu.com/' in url :
-        page.get(url)
-        page.wait(1)
-        connect = page.ele('@id=mp-editor').text
-        page.wait(1)
-        title = page.ele('@class=title-info-title').text
-        page.wait(1)
-        img = page.ele('@id=mp-editor').eles('tag:img')
-        imgList = img.get.links()
-        page.wait(1)
-        print('[{}] 文章标题、内容、图片获取成功'.format(nowTime()))
-        return connect, title, imgList[:8]
-    else:
-        return 5,5,5
+    except:
+        return 5, 5, 5
 
-def aiRewrite(article):
-    page.get('https://yiyan.baidu.com/')
-    page.set.load_mode.normal()
-    login = page.ele('xpath://*[@id="root"]/div[1]/div[2]/div/div[2]/div/div')
-    if login:
-        print('[{}] 请先登录，并且重新运行程序'.format(nowTime()))
-        page.wait(10)
-        sys.exit()
-    print('[{}] 开始AI改写......'.format(nowTime()))
-    page.ele('.yc-editor-paragraph').input(article)
-    page.wait(1)
-    ac.key_down(Keys.ENTER).key_up(Keys.ENTER)
-    page.wait(2)
-    page.wait.ele_hidden((page.ele('@id=sendBtn')).next(),timeout=1200)
-    page.wait(1)
-    print('[{}] 开始输入AI指令......'.format(nowTime()))
-    with open(r'AI指令.txt', 'r', encoding='utf-8') as file:
-        aiPromat = file.read()
-    page.ele('.yc-editor-paragraph').input(aiPromat)
-    page.wait(1)
-    ac.key_down(Keys.ENTER).key_up(Keys.ENTER)
-    page.wait(2)
-    page.wait.ele_hidden((page.ele('@id=sendBtn')).next(),timeout=1200)
-    page.wait(1)
-    page.ele('@id:chat-id-').eles('tag:span')[3].click()
-    print('[{}] AI洗稿成功'.format(nowTime()))
+def aiRewrite(article,aiType='wenxin'):
+    if aiType == 'wenxin':
+        page.get('https://yiyan.baidu.com/')
+        login = page.ele('xpath://*[@id="root"]/div[1]/div[2]/div/div[2]/div/div')
+        if login:
+            print('[{}] 请先登录，并且重新运行程序'.format(nowTime()))
+            page.wait(10)
+            sys.exit()
+        print('[{}] 开始AI改写......'.format(nowTime()))
+        page.ele('.yc-editor-paragraph').input(article)
+        page.wait(1)
+        ac.key_down(Keys.ENTER).key_up(Keys.ENTER)
+        page.wait(2)
+        page.wait.ele_hidden((page.ele('@id=sendBtn')).next(),timeout=120)
+        print('[{}] 开始输入AI指令......'.format(nowTime()))
+        with open(r'AI指令.txt', 'r', encoding='utf-8') as file:
+            aiPromat = file.read()
+        page.ele('.yc-editor-paragraph').input(aiPromat)
+        page.wait(1)
+        ac.key_down(Keys.ENTER).key_up(Keys.ENTER)
+        page.wait(2)
+        page.wait.ele_hidden((page.ele('@id=sendBtn')).next(),timeout=120)
+        page.wait(1)
+        page.ele('@id:chat-id-').eles('tag:span')[3].click()
+        print('[{}] AI洗稿成功'.format(nowTime()))
 
-def articleContrast(originalText):
+    elif aiType =='tongyi':
+        page.get('https://tongyi.aliyun.com/')
+        login = page.ele('xpath://*[@id="tongyiPageLayout"]/div[3]/div/div[1]/div/div/div[3]/button')
+        if login:
+            print('[{}] 请先登录，并且重新运行程序'.format(nowTime()))
+            page.wait(10)
+            sys.exit()
+        print('[{}] 开始AI改写......'.format(nowTime()))
+        page.ele('xpath://*[@id="tongyiPageLayout"]/div[3]/div/div[2]/div[1]/div[3]/div[4]/div[1]/div/textarea').input(article)
+        page.wait(1)
+        ac.key_down(Keys.ENTER).key_up(Keys.ENTER)
+        page.wait(2)
+        page.ele('xpath://*[@id="tongyiPageLayout"]/div[3]/div/div[2]/div[1]/div[2]/div[1]/div/div/div[2]/div[2]/div[2]/div[2]/div[3]',timeout=120)
+        print('[{}] 开始输入AI指令......'.format(nowTime()))
+        with open(r'AI指令.txt', 'r', encoding='utf-8') as file:
+            aiPromat = file.read()
+        page.ele('xpath://*[@id="tongyiPageLayout"]/div[3]/div/div[2]/div[1]/div[3]/div[4]/div[1]/div/textarea').input(aiPromat)
+        page.wait(1)
+        ac.key_down(Keys.ENTER).key_up(Keys.ENTER)
+        page.wait(2)
+        page.ele('xpath://*[@id="tongyiPageLayout"]/div[3]/div/div[2]/div[1]/div[2]/div[1]/div/div/div[4]/div[2]/div[2]/div[2]/div[3]',timeout=120).click()
+        print('[{}] AI洗稿成功'.format(nowTime()))
+
+def articleContrast(originalText,contrastType='meibp'):
     print('[{}] 开始文章检测......'.format(nowTime()))
-    page.get('http://www.wenpipi.com/sim')
-    updateBox = page.ele('@id=TurnOnScreenDialogBoxCloseId')
-    if updateBox:
-        updateBox.click()
-    page.wait(1)
-    page.ele('@id=content2').click()
-    ac.type(Keys.CTRL_V)
-    page.wait(1)
-    page.ele('@id=content1').input(originalText)
-    page.wait(1)
-    page.ele('@id=animation-container').click()
-    page.wait(1)
-    result = page.ele('xpath://*[@id="judgeDivId"]/font[2]/strong').text
+    if contrastType =='meibp':
+        page.get('https://ai.meibp.com/diff.html')
+        login = page.ele('xpath://*[@id="nav-container"]/div[2]/a')
+        if  login:
+            print('[{}] 请先登录，并且重新运行程序'.format(nowTime()))
+            page.wait(10)
+            sys.exit()
+        page.wait(1)
+        page.ele('xpath://*[@id="source2"]').click()
+        ac.type(Keys.CTRL_V)
+        page.wait(1)
+        page.ele('xpath://*[@id="source1"]').input(originalText)
+        page.wait(1)
+        page.ele('xpath://*[@id="button"]').click()
+        page.wait(1)
+        result = page.ele('xpath://*[@id="result"]/span').text
+        result = float(result[5:-2])
+        if result < 25.00:
+            return 0
+        else:
+            return 1
 
-    return result
+    elif contrastType =='wenpp':
+        page.get('http://www.wenpipi.com/sim')
+        updateBox = page.ele('@id=TurnOnScreenDialogBoxCloseId')
+        if updateBox:
+            updateBox.click()
+        page.wait(1)
+        page.ele('@id=content2').click()
+        ac.type(Keys.CTRL_V)
+        page.wait(1)
+        page.ele('@id=content1').input(originalText)
+        page.wait(1)
+        page.ele('@id=animation-container').click()
+        page.wait(1)
+        result = page.ele('xpath://*[@id="judgeDivId"]/font[2]/strong').text
+        if result == '原创' or result =='模仿(伪原创)':
+            return 0
+        else:
+            return 1
+
 
 if __name__=='__main__':
-
     secretKey()
     ac = Actions(page)
     page.set.window.max()
-
     lableLlist = {0:'自己输入文章链接',1:'热点',2:'娱乐',3:'体育',4:'军事',5:'历史',6:'财经'}
     print('[0]自己输入文章链接  [1]热点  [2]娱乐  [3]体育  [4]军事  [5]历史  [6]财经')
-    inputLable = int(input('请选择需要采集的文章领域(输入数字,如果不输入默认热点)：'))
+    inputLable = int(input('------->>>请选择:'))
     if inputLable > 6:
         inputLable = 1
     elif inputLable == 0 :
         with open(r'文章链接.txt', 'r', encoding='utf-8') as file:
-            url = file.read()
-        articleUrlList = url.splitlines()
+            fileUrl = file.read()
+        articleUrlList = fileUrl.splitlines()
     else:
         navItem = lableLlist[inputLable]
         articleUrlList = getArticleUrl(navItem = navItem)
+    print(articleUrlList)
 
     successNum = 0
     contrastNum = 0
     for url in articleUrlList:
         article,title,imgList = getArticle(url)
-        print(title,article,imgList)
+        # print(title,article,imgList)
         page.wait(1)
         if article == 5:
             print('[{}] 链接暂时不支持，自动跳过'.format(nowTime()))
             continue
-        aiRewrite(article)
+        aiRewrite(article,aiType='tongyi')
         page.wait(1)
 
         for i in range(3):
-            firm = articleContrast(article)
-            if firm == '原创' or firm =='模仿(伪原创)':
+            firm = articleContrast(article,contrastType='meibp')
+            if firm == 0 :
                 print('[{}] 文章检测完成'.format(nowTime()))
                 contrastNum = 0
                 break
@@ -233,4 +289,9 @@ if __name__=='__main__':
             pass
         successNum += 1
 
-        print('[{}] 头条成功写入: {} 条'.format(nowTime(),successNum))
+        if  inputLable == 0:
+            delUrl = fileUrl.replace(url,'')
+            with open(r'文章链接.txt', 'w', encoding='utf-8') as file:
+                file.write(delUrl)
+
+        print('[{}] {}写入成功，共成功 {} 条'.format(url,nowTime(),successNum))
