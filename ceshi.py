ad3eaa78bc7a1f40ea9f44d62f0454eb75716c42
czq411127520 <411127520@qq.com>
2024-11-01 10:14:13 +0800

    ctrl

--- ceshi.py
+++ ceshi.py
@@ -4,8 +4,16 @@ import pyautogui
 import tkinter as tk
 from tkinter import ttk
 import re 
-# page = ChromiumPage()
-# ac = Actions(page)
+page = ChromiumPage()
+ac = Actions(page)
+
+# page.ele('@id:kw').input((Keys.CTRL,'V'))
+# page.ele('tag:img').input((Keys.CTRL,'C'))
+
+aa = (page.ele('@class:ProseMirror')).child(1).click.at(0,0)
+
+# aa.input((Keys.HOME))
+ac.key_down(Keys.CTRL).type('V').key_up(Keys.CTRL)
 
 # connect = page.ele('xpath://*[@id="ArticleContent"]/div[2]/div').text
 # title = page.ele('xpath://*[@id="dc-normal-body"]/div[3]/div[1]/div[1]/div[2]/h1').text
@@ -15,30 +23,30 @@ import re
 # print(imgList)
 
 
-mainWindow = tk.Tk()
-mainWindow.title('自动化')
-mainWindow.geometry('600x800')
+# mainWindow = tk.Tk()
+# mainWindow.title('自动化')
+# mainWindow.geometry('600x800')
 
 
-releaseType = tk.StringVar(value='0')
-label = tk.Label(mainWindow, text="发布平台")
-label.pack()
-radio_button1 = tk.Radiobutton(mainWindow, text='发布平台',variable=releaseType,value='0',anchor='nw',padx=20, pady=20,)
-radio_button1.pack()
-radio_button2 = tk.Radiobutton(mainWindow, text='公众号',variable=releaseType,value='1',anchor='nw',padx=20, pady=20)
-radio_button2.pack()
+# releaseType = tk.StringVar(value='0')
+# label = tk.Label(mainWindow, text="发布平台")
+# label.pack()
+# radio_button1 = tk.Radiobutton(mainWindow, text='发布平台',variable=releaseType,value='0',anchor='nw',padx=20, pady=20,)
+# radio_button1.pack()
+# radio_button2 = tk.Radiobutton(mainWindow, text='公众号',variable=releaseType,value='1',anchor='nw',padx=20, pady=20)
+# radio_button2.pack()
 
-publishType = tk.StringVar(value='0')
-radio_button1 = tk.Radiobutton(mainWindow, text='头条',variable=publishType,value='0',anchor='w',padx=20, pady=20)
-radio_button1.pack(side=tk.LEFT)
-radio_button2 = tk.Radiobutton(mainWindow, text='公众号',variable=publishType,value='1',anchor='w',padx=20, pady=20)
-radio_button2.pack(side=tk.LEFT)
-radio_button3 = tk.Radiobutton(mainWindow, text='百家号',variable=publishType,value='2',anchor='w',padx=20, pady=20)
-radio_button3.pack(side=tk.LEFT)
+# publishType = tk.StringVar(value='0')
+# radio_button1 = tk.Radiobutton(mainWindow, text='头条',variable=publishType,value='0',anchor='w',padx=20, pady=20)
+# radio_button1.pack(side=tk.LEFT)
+# radio_button2 = tk.Radiobutton(mainWindow, text='公众号',variable=publishType,value='1',anchor='w',padx=20, pady=20)
+# radio_button2.pack(side=tk.LEFT)
+# radio_button3 = tk.Radiobutton(mainWindow, text='百家号',variable=publishType,value='2',anchor='w',padx=20, pady=20)
+# radio_button3.pack(side=tk.LEFT)
 
 
-button = tk.Button(mainWindow, text="点击我")
-button.pack()
+# button = tk.Button(mainWindow, text="点击我")
+# button.pack()
 
-mainWindow.mainloop()
+# mainWindow.mainloop()
 
